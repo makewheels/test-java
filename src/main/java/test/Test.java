592aa2d931e3e7fb6d790b724c65247111ef72ab
makewheels <makewheels>
2020-11-18 23:02:23 +0800

    加法

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,14 +5,14 @@ import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-        for (long g = 1; g <= 12; g++) {
+        for (long g = 1; g <= 5; g++) {
             Set<Long> result = new HashSet<Long>();
-            for (long j = 0; j <= 11; j++) {
-                long pow = (long) Math.pow(g, j);
-                result.add(pow % 13);
+            for (long j = 0; j <= 4; j++) {
+                long pow = g * j;
+                result.add(pow % 6);
             }
             System.out.print(g + ": " + result);
-            if (result.size() == 12) {
+            if (result.size() == 5) {
                 System.out.print(" yes");
             }
             System.out.println();
