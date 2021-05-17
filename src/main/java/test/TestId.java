9ee5cb6c4f880a6a1f9c57837b2703bae8f2493c
makewheels <makewheels>
2021-05-17 12:08:55 +0800

    snowflake

--- src/main/java/test/TestId.java
+++ src/main/java/test/TestId.java
@@ -1,4 +1,14 @@
 package test;
 
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
+
 public class TestId {
+    public static void main(String[] args) {
+        Snowflake snowflake = IdUtil.createSnowflake(1, 1);
+        for (int i = 0; i < 10; i++) {
+            Object id = snowflake.nextId();
+            System.out.println(id);
+        }
+    }
 }
