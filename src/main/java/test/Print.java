2521c239e3ff54c4a4154fd61a292c8afe504cc6
makewheels <makewheels>
2021-05-20 21:53:04 +0800

    print

---
+++ src/main/java/test/Print.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 200; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        for (Integer number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+}
