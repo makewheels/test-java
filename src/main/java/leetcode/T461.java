b11a517ba1dad923c245af8d1d57f0c0b522b2cf
makewheels <makewheels>
2021-05-27 22:16:08 +0800

    T461

---
+++ src/main/java/leetcode/T461.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class T461 {
+    public int hammingDistance(int x, int y) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new T461().hammingDistance(1, 4));
+    }
+}
