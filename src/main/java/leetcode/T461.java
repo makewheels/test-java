2c02f2b2b1c94ad1d88ba92c52798ee459af95fa
makewheels <makewheels>
2021-05-27 22:27:53 +0800

    T461

--- src/main/java/leetcode/T461.java
+++ src/main/java/leetcode/T461.java
@@ -2,7 +2,27 @@ package leetcode;
 
 public class T461 {
     public int hammingDistance(int x, int y) {
-        return 0;
+        String xBinary = Integer.toBinaryString(x);
+        String yBinary = Integer.toBinaryString(y);
+        int max = Math.max(xBinary.length(), yBinary.length());
+        int diff = Math.abs(xBinary.length() - yBinary.length());
+        StringBuilder addString = new StringBuilder();
+        for (int i = 0; i < diff; i++) {
+            addString.append("0");
+        }
+        if (xBinary.length() < max) {
+            xBinary = addString + xBinary;
+        } else {
+            yBinary = addString + yBinary;
+        }
+        int result = 0;
+        for (int i = 0; i < xBinary.length(); i++) {
+            char xChar = xBinary.charAt(i);
+            char yChar = yBinary.charAt(i);
+            if (xChar != yChar)
+                result++;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
