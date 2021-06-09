61248ec578275b9af8cc3574172f5220d482a33d
makewheels <makewheels>
2021-06-09 09:57:35 +0800

    T2

--- src/main/java/leetcode/T2.java
+++ src/main/java/leetcode/T2.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class T2 {
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
 
@@ -16,13 +16,13 @@ public class T2 {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        return l1;
     }
 
     public static void main(String[] args) {
-        T2.ListNode n3 = new T2.ListNode(3);
-        T2.ListNode n2 = new T2.ListNode(2, n3);
-        T2.ListNode n1 = new T2.ListNode(1, n2);
-        System.out.println(new T2().addTwoNumbers());
+        ListNode n3 = new ListNode(3);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode n1 = new ListNode(1, n2);
+        System.out.println(new T2().addTwoNumbers(n1, n2));
     }
 }
